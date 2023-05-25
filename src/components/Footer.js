829af84c157af57e054d9474d0ce000c6da0779d
Ben De Cruz <decruz_ben@hotmail.com>
2023-05-25 12:32:22 +1000

    finished home?

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,24 @@
 import "../css/Footer.css";
+import linkedInIco from "../img/linkedinico.png";
+import githubIco from "../img/githubico.png";
+import mailIco from "../img/mailico.png";
 
 function Footer() {
-  return <div className="footer-container">TBD Footer</div>;
+  return (
+    <div className="footer-container">
+      <div className="social-icons-container">
+        <a href="https://www.linkedin.com/in/bendecruz/">
+          <img src={linkedInIco} alt="Ben's LinkedIn profile" />
+        </a>
+        <a href="https://github.com/bendecruz745/">
+          <img src={githubIco} alt="Ben's Github profile" />
+        </a>
+        <a href="mailto:contact@bendecruz.com">
+          <img src={mailIco} alt="Send an email to Ben" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,29 @@ function Home() {
   return (
     <div className="home-container">
       <div className="home-content-container">
-        <div className="home-text-container"></div>
+        <div className="home-text-container">
+          <div className="home-intro-text-container text-bg">
+            <p className="home-intro-text">
+              Hello! I'm <br />
+              <b>Ben De Cruz</b>
+            </p>
+          </div>
+          <div className="home-body-text-container text-bg">
+            <p className="home-body-text-title">
+              <span>An</span> <span>Up</span> <span>&</span>
+              <span>Coming</span>
+              <span>
+                <b>Full Stack Developer</b>
+              </span>
+            </p>
+            <p className="home-body-text">
+              Welcome to my Portfolio site! Hopefully the content within shows
+              that my ability is suitable for the position you are looking to
+              fill, and that my projects can convey that ability when you check
+              them out.
+            </p>
+          </div>
+        </div>
         <img src={Ben} alt="It is indeed me, Ben" className="ben-picture" />
       </div>
     </div>
