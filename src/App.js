1efb9804e725990fcf3437314631403b596c97bd
Ben De Cruz <decruz_ben@hotmail.com>
2023-05-26 12:49:27 +1000

    updated home smallest width media query(380px) to be 460px

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="main-content">
         <NavBar />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home />} />
         </Routes>
         <Footer />
       </div>
