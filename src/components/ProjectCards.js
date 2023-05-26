080741fd678626ca1c7baea7cacfd3a6582d4c95
Ben De Cruz <decruz_ben@hotmail.com>
2023-05-26 17:42:19 +1000

    more updates on project cards

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -2,12 +2,17 @@ import basicChatAppPreviewSrc from "../img/BasicChatAppPreview.jpg";
 
 function ProjectCards({ projectInfo }) {
   return (
-    <div className="projects-card-container">
+    <div className="project-card-container">
       <img
         src={basicChatAppPreviewSrc}
         alt="Basic Chat App"
         className="project-preview-img"
       />
+      <div className="project-card-main-container">
+        <div className="project-card-title-container text-bg">
+          <h1>{projectInfo["title"]}</h1>
+        </div>
+      </div>
     </div>
   );
 }
