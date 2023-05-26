47a44d5566b90774f260e36f1fab4f2baf2f4de3
Ben De Cruz <decruz_ben@hotmail.com>
2023-05-26 11:30:49 +1000

    home page... done????? doubting

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,37 +4,30 @@ import Ben from "../img/selfienobg.png";
 function Home() {
   return (
     <div className="home-container">
-      <div className="home-content-container">
-        <div className="home-text-container">
-          <div className="home-intro-text-container text-bg">
-            <p className="home-intro-text">
-              Hello! I'm <br />
-              <b>Ben De Cruz</b>
-            </p>
-          </div>
-          <div className="home-body-text-container text-bg">
-            <p className="home-body-text-title">
-              <span className="hide-775px">An</span> <span>Up</span>{" "}
-              <span>&</span>
-              <span>Coming</span>
-              <span className="hide-775px">
-                <b>Full Stack Developer!</b>
-              </span>
-            </p>
-            <p className="home-body-text-title show-775px">
-              <b>Full Stack Developer!</b>
-            </p>
-
-            <p className="home-body-text">
-              Welcome to my Portfolio site! Hopefully this site & my projects
-              convey my abilities accurately to you, and I hope that they are
-              fit to the position you are looking to fill, or that I have
-              applied for.
-            </p>
-          </div>
+      <div className="home-text-container">
+        <div className="home-intro-text-container text-bg">
+          <p className="home-intro-text">
+            Hello! I'm <br />
+            <b>Ben De Cruz</b>,
+          </p>
         </div>
-        <img src={Ben} alt="It is indeed me, Ben" className="ben-picture" />
+        <div className="home-body-text-container text-bg">
+          <p className="home-body-text-title">
+            an up & coming <br />
+            <b>Full Stack Developer!</b>
+          </p>
+          <p className="home-body-text-title show-775px">
+            <b>Full Stack Developer!</b>
+          </p>
+
+          <p className="home-body-text">
+            Welcome to my Portfolio site! Hopefully this site & my projects
+            convey my abilities accurately to you, and I hope that they are fit
+            to the position you are looking to fill, or that I have applied for.
+          </p>
+        </div>
       </div>
+      <img src={Ben} alt="It is indeed me, Ben" className="ben-picture" />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,34 +8,38 @@ function NavBar() {
   };
   return (
     <div className="navbar-container">
-      <div className="navbar-brand-container">
-        <Link to="/" className="navbar-link-brand">
-          <h1 className="navbar-brand-text">Ben DC</h1>
-        </Link>
-      </div>
-      <div className="navbar-link-container">
-        <Link to="/Projects" className="navbar-link">
-          Projects
-        </Link>
-        <Link to="/About" className="navbar-link navbar-link-left-margin">
-          About
-        </Link>
-        <img
-          src={menuIco}
-          alt=""
-          className="navbar-burger-menu"
-          onClick={handleNavBurger}
-        />
-        <div className="navbar-burger-links-container">
-          <Link to="/Projects" className="navbar-burger-link">
+      <div className="navbar-content">
+        <div className="navbar-brand-container">
+          <Link to="/" className="navbar-link-brand">
+            <h1 className="navbar-brand-text">Ben DC</h1>
+          </Link>
+        </div>
+        <div className="navbar-link-container">
+          <Link to="/Projects" className="navbar-link">
             Projects
           </Link>
-          <Link
-            to="/About"
-            className="navbar-burger-link navbar-link-left-margin"
-          >
+          <Link to="/About" className="navbar-link navbar-link-left-margin">
             About
           </Link>
+          <div className="navbar-burger-container">
+            <img
+              src={menuIco}
+              alt=""
+              className="navbar-burger-menu-button"
+              onClick={handleNavBurger}
+            />
+            <div className="navbar-burger-links-container">
+              <Link to="/Projects" className="navbar-burger-link">
+                Projects
+              </Link>
+              <Link
+                to="/About"
+                className="navbar-burger-link navbar-link-left-margin"
+              >
+                About
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
