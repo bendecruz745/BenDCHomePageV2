a6e430421916c04a2b30846f67a4741de7b16e52
Ben De Cruz <decruz_ben@hotmail.com>
2023-05-30 16:57:25 +1000

    basic about page for now, should prettify it later

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,84 @@
 import "../css/About.css";
 
 function About() {
-  return <div className="about-page-container">Under construction</div>;
+  return (
+    <div className="about-page-container">
+      <div className="about-window-container text-bg">
+        <div className="about-window-title-container">
+          <h1 className="about-window-title-text">About Me</h1>
+        </div>
+        <div className="about-window-main-text-container">
+          <p>
+            Hello, I'm Ben (probably already picked that up), a fledgling Full
+            Stack Developer.
+          </p>
+          <p>
+            I've had an interest in computers since I was introduced to them at
+            a young age, as I was always amazed at what they are able to do and
+            what people can do with them.
+          </p>
+          <p>
+            I did not go to University due to lacking requirements, however a
+            few months after finishing High School in February 2015, I somehow
+            had the luck and privilege of being hired on as an Intern at the
+            Reserve Bank of Australia for their 2 year IT Intern program. This
+            was a life changing event for me.
+          </p>
+          <p>
+            Mostly because of the life-long friends I made from some of the
+            other Interns, but also of the experience and knowledge I was able
+            to accumulate when rotating through their Service Desk, Network
+            Services, System Services (server management), IT Operations and
+            Security teams.
+          </p>
+          <p>
+            A few months after finishing up my Internship on Feb 2017, on May
+            2017 I was hired on as a temp IT Support Officer at Parker Hannifin
+            Australia, moving onto a salary role on Sep 2017.
+          </p>
+          <p>
+            My role at Parker Hannifin's Australian branch ended up being a type
+            of jack-of-all-trades role, covering level 1-3 support. I was first
+            in line for phone calls and the main person managing the ticket
+            queue. Issues I looked at could range from hardware & software,
+            server, telecommunication, networking, etc.
+          </p>
+          <p>
+            During my time at Parker, due to (in my opinion) a small IT Support
+            team (Network Administrator, IT Manager & Myself), looking after
+            branches all over Australia & a few in New Zealand led to some
+            straining and high pressure situations.
+          </p>
+          <p>
+            However, this time has sharpened my problem solving ability and
+            reaction time, and shaped my current work ethic as being persistent.
+            It exposed me to many problems from many angles and to be honest, it
+            sharpened my Googling skills big time for finding solutions to those
+            very odd issues that pop up here and there.
+          </p>
+          <p>
+            I started undertaking Australian Institute of ICT's Certified Full
+            Stack Developer course around August 2022 and decided to leave
+            Parker on September 2022 to dedicate myself to the course and also
+            partly a mental break.
+          </p>
+          <p>
+            I finished the course on Apr 2023 and have since started working on
+            some personal projects here and there to build up my knowledge &
+            practical ability further, whilst applying to some jobs here and
+            there when they pop up. (You are likely here as you may have seen
+            the link on my LinkedIn or on my resume)
+          </p>
+          <p>
+            That is pretty much where I am at the moment. If you would like my
+            Resume, send me an email via the mail link below, or if you already
+            have it and are looking for a Full Stack Developer, hopefully I am
+            what you are looking for!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
